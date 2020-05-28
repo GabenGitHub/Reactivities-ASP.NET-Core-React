a0c90bb77ace3017e4fab1554680949caa1566c9
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-05-28 10:07:09 +0200

    Refactor App class component using React Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Header, Icon } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
 
-const App: React.FC = () => {
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+          setActivities(response.data)
+      });
+  }, []);
+
   return (
     <div className="App">
       <Header as='h2'>
         <Icon name='users' />
         <Header.Content>Reactivities</Header.Content>
       </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
     </div>
   );
 }
