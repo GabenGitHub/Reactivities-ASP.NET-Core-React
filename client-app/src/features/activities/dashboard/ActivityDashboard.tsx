eaede05143f999f52b154ec5b9e2a971a796e66a
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-06-04 00:39:35 +0200

    Add Selecting individual activity functionality

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,16 +7,18 @@ import ActivityFrom from '../form/ActivityFrom';
 
 interface IProps {
     activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+const ActivityDashboard: React.FC<IProps> = ({activities, selectActivity, selectedActivity}) => {
     return (
         <Grid>
             <Grid.Column width={10} >
-                <ActivityList activities={activities}/>
+                <ActivityList activities={activities} selectActivity={selectActivity}/>
             </Grid.Column>
             <Grid.Column width={6}>
-                <ActivityDetails />
+                {selectedActivity && <ActivityDetails activity={selectedActivity}/> }
                 <ActivityFrom />
             </Grid.Column>
         </Grid>
